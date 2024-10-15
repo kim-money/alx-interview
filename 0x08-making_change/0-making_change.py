06dd3eaaf6fca8ba7addefa238ad020f69233af3
moses_kim <wangingimoses102@gmail.com>
2024-10-15 10:16:20 +0300

    corrections for 08

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Given a pile of coins of different values, determine the
+   fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """Return: fewest number of coins needed to meet total"""
+    if total <= 0:
+        return 0
+    if not coins or len(coins) == 0:
+        return -1
+
+    change = 0
+    available_coins = sorted(coins, reverse=True)
+    change_left = total
+
+    for coin in available_coins:
+        while (change_left % coin >= 0 and change_left >= coin):
+            change += int(change_left / coin)
+            change_left = change_left % coin
+
+    change = change if change_left == 0 else -1
+    return change
